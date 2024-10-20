81b61db3def7af47d862ef442e8d0706c373b68a
Nguyen Thanh Trung <trunghy2k4111@gmail.com>
2024-10-20 22:28:03 +0700

    code card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em iuuuu';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Anh xin lỗi đã làm em phiền lòng nhiều !';
+const contentCard = 'Chúc em iuu của anh 20/10 luôn luôn xinh đẹp và học thật giỏi nhá .Phải luôn lạc quan' +
+    ' vui vẻ nha .Cố gắng lên nhá , Fighting !'     ;
+
+const giftImage = 'my.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
